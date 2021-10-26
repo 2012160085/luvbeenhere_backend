de7407109b7b02ce65910b267ee72c1ce4c62340
kibeom <lkbest0085@google.com>
2021-10-26 13:45:56 +0900

    createVisitResponse 추가

--- App/src/visits/createVisit/createVisit.resolvers.ts
+++ App/src/visits/createVisit/createVisit.resolvers.ts
@@ -1,108 +1,114 @@
-import { averageInArr, date2StrDay, minmaxDateInArr, uploadPhoto } from "../../shared/shared.utils";
+import {
+  averageInArr,
+  date2StrDay,
+  minmaxDateInArr,
+  uploadPhoto,
+} from "../../shared/shared.utils";
 import { Resolvers } from "../../types";
-import AWS from 'aws-sdk'
+import AWS from "aws-sdk";
 import { protectedResolver } from "../../users/users.utils";
 const resolvers: Resolvers = {
-    Mutation: {
-        createVisit: protectedResolver
-            (async (_, {
-                name,
-                placeId,
-                photoPosts,
-                rating,
-                comment,
-                datetime,
-                advantage,
-                shortage,
-                price,
-                isPublic
-            }, { client, loggedInUser }) => {
+  Mutation: {
+    createVisit: protectedResolver(
+      async (
+        _,
+        {
+          name,
+          placeId,
+          photoPosts,
+          rating,
+          comment,
+          datetime,
+          advantage,
+          shortage,
+          price,
+          isPublic,
+        },
+        { client, loggedInUser }
+      ) => {
+        //사진 업로드
+        const uploadPromises = photoPosts.map((pp) => uploadPhoto(pp.file));
+        const photoUrls = (await Promise.all(uploadPromises)).map((upload) => [
+          upload["Location"],
+        ]);
 
-                //사진 업로드
-                const uploadPromises = photoPosts.map(pp => uploadPhoto(pp.file))
-                const photoUrls = (await Promise.all(uploadPromises)).map(upload => [upload["Location"]])
+        const photoInfo = photoPosts.map((pp, index) => {
+          pp["file"] = photoUrls[index][0];
+          pp["datetime"] = new Date(pp["datetime"]);
 
-                const photoInfo = photoPosts.map((pp, index) => {
-                    pp['file'] = photoUrls[index][0]
-                    pp['datetime'] = new Date(pp['datetime'])
+          return { ...pp };
+        });
 
-                    return { ...pp }
-                })
+        const minmaxDate = minmaxDateInArr(
+          photoInfo.map((pi) => pi["datetime"])
+        );
 
-                const minmaxDate = minmaxDateInArr(
-                    photoInfo.map(pi => pi['datetime'])
-                )
+        const strDateDay = date2StrDay(minmaxDate[0]);
+        const avgPosX = averageInArr(photoInfo.map((pi) => pi["posX"]));
+        const avgPosY = averageInArr(photoInfo.map((pi) => pi["posY"]));
+        console.log(strDateDay);
+        //데이트 없으면 생성
+        var date = await client.mDate.findUnique({
+          where: {
+            datetime_coupleId: {
+              datetime: new Date(strDateDay).toISOString(),
+              coupleId: loggedInUser.coupleId,
+            },
+          },
+        });
+        if (!date) {
+          date = await client.mDate.create({
+            data: {
+              datetime: new Date(strDateDay).toISOString(),
+              name: `${strDateDay} 데이트`,
+              couple: {
+                connect: {
+                  id: loggedInUser.coupleId,
+                },
+              },
+            },
+          });
+        }
 
-                const strDateDay = date2StrDay(minmaxDate[0])
-                const avgPosX = averageInArr(photoInfo.map(pi => pi['posX']))
-                const avgPosY = averageInArr(photoInfo.map(pi => pi['posY']))
-                console.log(strDateDay);
-                //데이트 없으면 생성
-                var date = await client.mDate.findUnique({
-                    where: {
-                        datetime_coupleId: {
-                            datetime: new Date(strDateDay).toISOString(),
-                            coupleId: loggedInUser.coupleId
-                        }
-                    }
-                })
-                if (!date) {
-                    date = await client.mDate.create({
-                        data: {
-                            datetime: new Date(strDateDay).toISOString(),
-                            name: `${strDateDay} 데이트`,
-                            couple: {
-                                connect: {
-                                    id: loggedInUser.coupleId
-                                }
-                            }
-                        }
-                    })
-                }
+        //방문 생성
+        const createdVisit = await client.visit.create({
+          data: {
+            name,
+            date: {
+              connect: {
+                id: date.id,
+              },
+            },
+            photos: {
+              createMany: {
+                data: photoInfo,
+              },
+            },
+            datetime: new Date(strDateDay).toISOString(),
+            advantage,
+            comment,
+            isPublic,
+            ...(rating && {
+              rating: {
+                create: {
+                  value: rating,
+                },
+              },
+            }),
+            posX: avgPosX,
+            posY: avgPosY,
+            price,
+            shortage,
+          },
+        });
 
-
-
-                //방문 생성
-                await client.visit.create({
-                    data: {
-                        name,
-                        date: {
-                            connect: {
-                                id: date.id
-                            }
-                        },
-                        photos: {
-                            createMany: {
-                                data: photoInfo
-                            }
-                        },
-                        datetime: new Date(strDateDay).toISOString(),
-                        advantage,
-                        comment,
-                        isPublic,
-                        ...(rating && {
-                            rating: {
-                                create: {
-                                    value: rating,
-                                }
-                            }
-                        }),
-                        posX: avgPosX,
-                        posY: avgPosY,
-                        price,
-                        shortage, 
-                    }
-                })
-
-
-
-
-
-                return {
-                    ok: true
-                }
-            })
-    }
-}
+        return {
+          ok: true,
+          visitId: createdVisit.id,
+        };
+      }
+    ),
+  },
+};
 
 export default resolvers;
--- App/src/visits/createVisit/createVisit.typeDefs.js
+++ App/src/visits/createVisit/createVisit.typeDefs.js
@@ -1,18 +1,23 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-    type Mutation{
-        createVisit(
-            name      :String!
-            placeId   :Int
-            photoPosts    :[PhotoPost]!
-            rating    :Int
-            comment   :String
-            advantage :String
-            shortage  :String
-            price     :Int
-            isPublic  :Boolean
-            datetime  :String
-        ): MutationResponse!
-    }
+  type CreateVisitResponse {
+    ok: Boolean!
+    error: String
+    visitId: Int
+  }
+  type Mutation {
+    createVisit(
+      name: String!
+      placeId: Int
+      photoPosts: [PhotoPost]!
+      rating: Int
+      comment: String
+      advantage: String
+      shortage: String
+      price: Int
+      isPublic: Boolean
+      datetime: String
+    ): MutationResponse!
+  }
 `;
