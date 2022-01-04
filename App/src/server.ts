237300e5a6741f55a1cb1ead6115fc8fb3550ae9
kibeom <lkbest0085@gmail.com>
2022-01-04 06:09:54 +0000

    test

--- App/src/server.ts
+++ App/src/server.ts
@@ -1,15 +1,16 @@
 require("dotenv").config();
 import http from 'http';
-import express from "express";
+import express, { application } from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import client from "./client";
 import { getUser } from "./users/users.utils";
-
+import { graphqlUploadExpress } from 'graphql-upload'
 const apollo = new ApolloServer({
     typeDefs,
     resolvers,
+    uploads: false,
     context: async ({ req }) => {
         return {
             loggedInUser: await getUser(req.headers.token),
@@ -34,13 +35,10 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(logger("common"))
+app.use(graphqlUploadExpress());
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
 
-const httpServer = http.createServer(app);
-apollo.installSubscriptionHandlers(httpServer);
-
-
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
